2a0be507beaa2e2bc7bc64efefcada89e8d3bd72
sjia <enluan.jia@outlook.com>
2020-02-19 23:42:31 +0800

    configure static files

--- sjia/urls.py
+++ sjia/urls.py
@@ -2,6 +2,7 @@ from django.urls import path, include
 import sjia.views
 
 urlpatterns = [
+    path('', sjia.views.index),
     path('index', sjia.views.index),
     path('results', sjia.views.results_log),
     path('chart', sjia.views.chart)
